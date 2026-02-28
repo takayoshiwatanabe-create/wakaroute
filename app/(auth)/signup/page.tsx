3fa4e286c0baea15537dc1a98fe0a6e30fd027d7
takayoshiwatanabe-create <takayoshi.watanabe@ruok.jp>
2026-03-01 00:23:41 +0900

    fix: put use client before dynamic export

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic';
-
 "use client";
+
+export const dynamic = 'force-dynamic';
 
 import { SignupForm } from "@/components/auth/signup-form";
 import { useTranslations } from "next-intl";
